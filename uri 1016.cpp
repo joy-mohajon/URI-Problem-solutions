991135641f9d930480f731ed9f63b100624fcbbd
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:06 -0600

    Add uri 1016.cpp

---
+++ uri 1016.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    //displaying minutes by multiply 2
+    cout << a*2 << " minutos" << endl;
+
+    return 0;
+}
+

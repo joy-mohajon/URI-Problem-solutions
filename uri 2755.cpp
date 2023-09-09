9f45d32e4ef8fbc775687e07ba517980e7a8ad30
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:15 -0600

    Add uri 2755.cpp

---
+++ uri 2755.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // displaying some massage ...
+    cout << "\"Ro\'b\'er\tto\\\/\""<< endl;
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << endl;
+    cout << "(^_-) (-_-) (-_^)" << endl;
+    cout << "(\"_\") (\'.\')" << endl;
+
+    return 0;
+}
+

d6998fbed685011ee801da9c80d3304ed14f3c19
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:26 -0600

    Add uri 2936.cpp

---
+++ uri 2936.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, s;
+
+    // the portions that will consume
+    cin >> a >> b >> c >> d >> e;
+
+    // the calculating the measure of cassava
+    s=(a*300+b*1500+c*600+d*1000+e*150);
+
+    cout << s+225 << endl;
+
+    return 0;
+}
+

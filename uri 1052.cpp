63ee9777b335473c82aeef0cf2378e218ad63c3a
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:20 -0600

    Add uri 1052.cpp

---
+++ uri 1052.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int d;
+    cin >> d;
+
+    /* displaying d'th (d means input
+    variable) name of the month */
+
+    if (d==1)
+        cout << "January\n";
+    else if (d==2)
+        cout << "February\n";
+    else if (d==3)
+        cout << "March\n";
+    else if (d==4)
+        cout << "April\n";
+    else if (d==5)
+        cout << "May\n";
+    else if (d==6)
+        cout << "June\n";
+    else if (d==7)
+        cout << "July\n";
+    else if (d==8)
+        cout << "August\n";
+    else if (d==9)
+        cout << "September\n";
+    else if (d==10)
+        cout << "October\n";
+    else if (d==11)
+        cout << "November\n";
+    else if (d==12)
+        cout << "December\n";
+
+    return 0;
+}
+

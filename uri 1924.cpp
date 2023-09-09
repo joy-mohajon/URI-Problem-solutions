84a09f293918569b450f5b6fe47998ad3d48e427
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:27 -0600

    Add uri 1924.cpp

---
+++ uri 1924.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int s=0, n, i;
+
+    char str[100];
+
+    // indicate how many string will take input
+    cin >> n;
+    scanf("%*c");
+
+    // input string
+    while (n--)
+    {
+        gets(str);
+    }
+
+    // displaying the name of the course
+    cout << "Ciencia da Computacao" << endl;
+
+    return 0;
+}

a2157a89f7090c326e426a28a62cf68263f6878b
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:12 -0600

    Add uri 1387.cpp

---
+++ uri 1387.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while(1)
+    {
+        cin >> a >> b;
+
+        // exit point
+        if(a==0 && b==0)
+            break;
+
+        // displaying sum of them
+        cout << a+b << endl;
+    }
+
+    return 0;
+}
+
+
+

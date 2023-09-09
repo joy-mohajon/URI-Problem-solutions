c376d25d5e961c264b7e8ca85723cb562b5cee6b
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:01 -0600

    Add uri 1007.cpp

---
+++ uri 1007.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, DIFERENCA;
+
+    //input 4 integer number
+    cin >> A >> B >> C >> D;
+
+    //calculating difference of products
+    DIFERENCA = A * B - C * D;
+
+    //displaying difference
+    cout << "DIFERENCA = " << DIFERENCA << endl;
+
+    return 0;
+}
+
+

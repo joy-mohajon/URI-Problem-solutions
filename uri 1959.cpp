122ab8a598d8b6a3962c5964fbc21f471ab13a52
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:31 -0600

    Add uri 1959.cpp

---
+++ uri 1959.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    long long int a, b, s;
+
+    cin >> a >> b;
+
+    // calculating multiply of them
+    s = a * b;
+
+    // displaying result of multiply
+    cout << s << endl;
+
+    return 0;
+}
+

708564623aef1ae975b5a752be6f87c0c3b5ceff
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:00 -0600

    Add uri 1184.cpp

---
+++ uri 1184.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+main()
+
+{
+    int i, j;
+    char c;
+    double arr[12][12], s=0.0;
+
+    // input a character
+    cin >> c;
+
+    // input arrays values
+    for (i=0; i<12; i++)
+    {
+        for (j=0; j<12; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // calculating sum
+    for (i=0; i<12; i++)
+    {
+        for (j=i+1; j<12; j++)
+        {
+            s+=arr[j][i];
+        }
+    }
+
+    cout << fixed;
+
+    // displaying sum or average
+    if (c == 'S')
+        cout << setprecision(1) << s << endl;
+
+    else if (c == 'M')
+        cout << setprecision(1) << s/66.0 << endl;
+
+
+    return 0;
+
+
+}
+
+
+

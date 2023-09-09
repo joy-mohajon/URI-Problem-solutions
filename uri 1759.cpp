424934d0a73b3f145c72707cb006e88f6aee9875
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:20 -0600

    Add uri 1759.cpp

---
+++ uri 1759.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    // indicate how times
+    cin >> n;
+
+    // displaying ...
+    cout << "Ho" ;
+
+    for (i=0; i<n-2; i++)
+    {
+        cout << " Ho" ;
+    }
+    cout << " Ho!" << endl;
+
+    return 0;
+}
+

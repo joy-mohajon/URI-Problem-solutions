f2b46930617df93a4799ba28c4155f9a2b0491d7
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:44 -0600

    Add uri 2146.cpp

---
+++ uri 2146.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, pass;
+
+    // input number for end of file
+    while(cin >> n)
+    {
+        // calculating password
+        pass = n - 1;
+
+        // displaying ...
+        cout << pass << endl;
+    }
+
+    return 0;
+
+}
+

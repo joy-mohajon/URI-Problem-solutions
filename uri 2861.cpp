352b750a47a9124e84272b17fa2b509426828f06
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:24 -0600

    Add uri 2861.cpp

---
+++ uri 2861.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    // test case
+    cin >> n;
+    scanf("%*c");
+
+    string st;
+
+    // input a question and
+    //displaying a answer
+    for(i=0; i<n; i++)
+    {
+        getline(cin, st);
+        printf("gzuz\n");
+    }
+
+    return 0;
+}

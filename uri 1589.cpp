7a994b9ac1e1be0e775ea3cafd51b4c09a6b224a
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:19 -0600

    Add uri 1589.cpp

---
+++ uri 1589.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, s, i, a, b;
+
+    cin >> n;
+
+    for (i=0; i<n; i++)
+    {
+        cin >> a >> b;
+
+        // calculating ...
+        s = a + b;
+
+        // displaying ...
+        cout << s << endl;
+
+    }
+
+    return 0;
+}
+
+

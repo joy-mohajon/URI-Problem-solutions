226098763d22960e7615c9e0266ff6136942f6a7
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:27 -0600

    Add uri 1921.cpp

---
+++ uri 1921.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int a,n;
+
+    // input the number of sides
+    cin >> n;
+
+    // calculating ...
+    a=(n * (n - 3) ) / 2;
+
+    // displaying the number of string
+    cout << a << endl;
+
+    return 0;
+}
+

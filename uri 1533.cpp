8aa7ec789851e23e33a9ec2b605dba690bd1e3df
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:16 -0600

    Add uri 1533.cpp

---
+++ uri 1533.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, temp;
+
+    while(scanf("%d",&n))
+    {
+        if(n==0)
+           break;
+
+        int suspect[n], index[n];
+
+        // input the value of suspects
+        //and create their index
+        for(i=0; i<n; i++)
+        {
+            cin >> suspect[i];
+            index[i]=i+1;
+        }
+
+        /* sorting suspect values
+        with their index values
+        by ascending order  */
+        for(i=0; i<n; i++)
+        {
+            for(j=i+1; j<n; j++)
+            {
+                if(suspect[i]>suspect[j])
+                {
+                    temp=suspect[i];
+                    suspect[i]=suspect[j];
+                    suspect[j]=temp;
+
+                    temp= index[i];
+                    index[i]= index[j];
+                    index[j]=temp;
+                }
+            }
+        }
+
+        // displaying the index of the killer
+        cout << index[n-2] << endl;
+
+    }
+
+    return 0;
+}

329a4034ce449ae3bc6d1673bc7012958eabebd3
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:19 -0600

    Add uri 1564.cpp

---
+++ uri 1564.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        // displaying according to the question
+        if (n==0)
+            cout << "vai ter copa!" << endl;
+        else
+            cout << "vai ter duas!" << endl;
+    }
+    return 0;
+}
+

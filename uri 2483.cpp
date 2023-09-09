677ee24363e8ab326aebefb259708e56b83a070c
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:00 -0600

    Add uri 2483.cpp

---
+++ uri 2483.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i, n;
+
+    cin >> n;
+
+    // displaying a message
+    cout << "Feliz nat";
+
+    // displaying a character of the
+    // message depend on input value
+    for (i=0; i<n; i++)
+    {
+        cout << "a";
+    }
+    cout << "l!" << endl;
+
+    return 0;
+}

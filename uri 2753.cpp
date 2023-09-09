e9bdaa833b537635a19e90ebbd75182ab1425d7d
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:14 -0600

    Add uri 2753.cpp

---
+++ uri 2753.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+
+    // displaying a to z with
+    // their ascii value
+    for (i=97; i<123; i++)
+    {
+        printf("%d e %c\n",i,i);
+    }
+
+    return 0;
+}
+
+
+

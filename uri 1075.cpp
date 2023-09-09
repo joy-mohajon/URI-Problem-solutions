73d4fad72cc9bf2671e4fe3eaf13894af8a0c4be
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:28 -0600

    Add uri 1075.cpp

---
+++ uri 1075.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int r,i;
+    cin>>r;
+
+    for (i=2; i<=10000; i=i+r)
+    {
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
+
+

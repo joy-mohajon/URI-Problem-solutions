d5c20a4ef0974eabb25d8ecbffecf40a344be2bb
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:05 -0600

    Add uri 2544.cpp

---
+++ uri 2544.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while(cin >> n)
+    {
+        // calculating and displaying the
+        //number of times using log2 function
+        int a = log2(n);
+        cout << a << endl;
+    }
+    return 0;
+}
+

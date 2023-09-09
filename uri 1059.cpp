d7333709b281f25b1374d42b530e3218c777dcd7
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:21 -0600

    Add uri 1059.cpp

---
+++ uri 1059.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //find out and displaying even number
+
+    for (int i=1; i<=100; i++)
+    {
+        if (i%2==0)
+            cout<<i<<endl;
+    }
+    return 0;
+}
+

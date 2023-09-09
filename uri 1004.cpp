78dbb9246cb17e4356edb4ca325f954e762f7807
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:42:59 -0600

    Add uri 1004.cpp

---
+++ uri 1004.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int A,B,PROD;
+
+    //input 2 integer number
+    cin >> A >> B;
+
+    //calculate product of 2 numbers
+    PROD = A * B;
+
+    //displaying product of 2 numbers
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
+

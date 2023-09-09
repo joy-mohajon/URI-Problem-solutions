5d38af734083297369affb069d5931d767120e9c
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:45:07 -0600

    Add uri 2581.cpp

---
+++ uri 2581.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string str;
+
+    cin >> n;
+
+    while(n--)
+    {
+        // to remove a new line
+        scanf("%*c");
+        // input a questions
+        getline(cin, str);
+
+        // answer of the question
+        cout << "I am Toorg!" << endl;
+    }
+
+    return 0;
+}
+

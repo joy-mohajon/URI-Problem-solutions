e4615dfa8f540d02e93350612720905ea8baaf0e
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:43:43 -0600

    Add uri 1143.cpp

---
+++ uri 1143.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j=1;
+
+    cin >> n;
+
+    // displayig sequence
+
+    for (i=1; i<=n; i++)
+
+        cout << i << " " << i*i << " " << i*i*i << endl;
+
+
+    return 0;
+}
+
+

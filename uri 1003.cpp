36dc29faadbd276d07ad71f86f9fc86c209a6b8e
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:42:59 -0600

    Add uri 1003.cpp

---
+++ uri 1003.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int A, B, SOMA;
+
+    //input 2 integer number
+    cin >> A >> B;
+
+    //calculate sum of 2 numbers
+    SOMA = A + B;
+
+    //displaying sum of 2 numbers
+    cout << "SOMA = " << SOMA << endl;
+
+    return 0;
+
+}
+

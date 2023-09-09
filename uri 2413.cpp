811a35d6975d0f66eccf2a69700e72669704a642
Joy Mohajon <92334273+joy-mohajon@users.noreply.github.com>
2023-09-09 14:44:59 -0600

    Add uri 2413.cpp

---
+++ uri 2413.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    /* we can get the number of
+    the people by multiply 4
+    with input number  */
+
+    cin >> n;
+
+    cout << n*4 << endl;
+
+    return 0;
+}
+
